35c22c5fbe7cefcdc4703ad10042ec19ecc6de54
ariffridasari <arif.fridasari@students.amikom.ac.id>
2019-05-17 10:42:39 +0700

    Arif Fridasari Mengedit Document dan Testing

--- test/tugas1/Nomor1Test.java
+++ test/tugas1/Nomor1Test.java
@@ -17,18 +17,21 @@ public class Nomor1Test {
     public Nomor1Test() {
     }
 
-    /**
-     * @Arif Fridasari
-     * Test of kombinasi method, of class Nomor1.
-     */
-    
     @Test
      public void test1() {
          Nomor1 obj = new Nomor1();
+         /**
+          * Create : Arif Fridasari
+          * Test Untuk Pemangkatan
+          */
          assertEquals(625.0, obj.pangkat(5.0, 4.0), 0);
          assertEquals(0.125, obj.pangkat(2.0, -3.0), 0);
          assertEquals(81.0, obj.pangkat(9.0, 2.0), 0);
          
+         /**
+          * Create : Arif Fridasari
+          * Test Untuk Kombinasi
+          */
          assertEquals(5, obj.kombinasi(5, 4));
          assertEquals(35, obj.kombinasi(7, 3));
          assertEquals(84, obj.kombinasi(9, 3));
