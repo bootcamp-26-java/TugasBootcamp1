880f9dd7c95cc35c16a990e3d0afb257647a870d
sindisyw <sindiyuliawibowo31@gmail.com>
2019-05-17 08:06:09 +0700

    sindi menambahkan auto increment dn test package increment

--- test/tugas1/Nomor1Test.java
+++ test/tugas1/Nomor1Test.java
@@ -3,22 +3,25 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package tugas1;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import tugas1.Nomor1;
 
 /**
  *
- * @author KHAIRUL MUNA
+ * @author HP
  */
-public class TestNomor1 {
-    Nomor1 nomor1;
-    
-    public TestNomor1() {
+public class Nomor1Test {
+
+    public Nomor1Test() {
     }
+
+    /**
+     * Test of kombinasi method, of class Nomor1.
+     */
     
-     @Test
+    @Test
      public void test1() {
          Nomor1 obj = new Nomor1();
          assertEquals(625.0, obj.pangkat(5.0, 4.0), 0);
@@ -29,4 +32,11 @@ public class TestNomor1 {
          assertEquals(35, obj.kombinasi(7, 3));
          assertEquals(84, obj.kombinasi(9, 3));
      }
+    /**
+     * Test of main method, of class Nomor1.
+     */
+    @Test
+    public void testMain() {
+    }
+
 }
