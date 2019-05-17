b4199ee2a1205a6333758720a63748249b8ba8db
gdwiweka <wiwekananda16@gmail.com>
2019-05-17 08:41:43 +0700

    Revisi testnomo2

--- test/tugas1/Nomor2Test.java
+++ test/tugas1/Nomor2Test.java
@@ -1,25 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tugas1;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import tugas1.Nomor2;
 
 /**
  *
  * @author Gede
  */
 public class Nomor2Test {
-    
-    public Nomor2Test() {
+    Nomor2 coba = new Nomor2();
+    @Test
+    public void testDecimaltoBinary() {
+        assertEquals("1100100", coba.DecimaltoBinary(100));
+        assertEquals("1111101000", coba.DecimaltoBinary(1000));
+        assertEquals("1111111111111111111111111111111111111111111111111111111111111111", coba.DecimaltoBinary(-1));
+        
     }
-
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    
+    @Test
+    public void testDecimaltoHexa() {
+        assertEquals("96", coba.DecimaltoHexa(150));
+        assertEquals("3E8", coba.DecimaltoHexa(1000));
+        assertEquals("FFFFFFFFFFFFFFFF", coba.DecimaltoHexa(-9));
+    }
+    @Test
+    public void testDecimaltoOctal() {
+        assertEquals(37, coba.DecimaltoOctal(31));
+        assertEquals(1750, coba.DecimaltoOctal(1000));
+        assertEquals(1, coba.DecimaltoOctal(-1)); 
+    }
+    @Test
+    public void testBinarytoDecimal() {
+        assertEquals(1, coba.BinerToDecimal(1));
+        assertEquals(8, coba.BinerToDecimal(1000));
+    }
+    @Test
+    public void testHexatoDecimal() {
+        assertEquals(158, coba.HexaToDecimal("9E"));  
+        assertEquals(2538, coba.HexaToDecimal("9EA"));
+        assertEquals(-1, coba.HexaToDecimal("FFFFFFFFFFFFFFFF"));
+    }
+    @Test
+    public void testOctaltoDecimal() {
+        assertEquals(31, coba.OctalToDecimal(37));
+        assertEquals(512, coba.OctalToDecimal(1000));
+        assertEquals(1, coba.OctalToDecimal(-1));
+    }
+    
 }
