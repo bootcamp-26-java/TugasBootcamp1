ba389ab4dd4c97f23523c3e29a67d280198cde64
gdwiweka <wiwekananda16@gmail.com>
2019-05-17 10:27:55 +0700

    Revisi Nomor 1

--- src/tugas1/Nomor1.java
+++ src/tugas1/Nomor1.java
@@ -2,6 +2,7 @@
  * Arif Fridasari Mengerjakan Nomor 1 Bagian Method Pemangkatan dan Kombinasi;
  */
 package tugas1;
+
 /**
  * Class Nomor1 yang digunakan untuk membuat nilai kembali dengan dua variabel a
  * dan b untuk menentukan nilai kombinasi dan perpangkatan dengan nama fungsi
@@ -19,22 +20,24 @@ public class Nomor1 {
      * @param b
      * @return
      */
-    public int kombinasi(int a, int b) {
-        int awal = 1;
-        int btsbwh = 1;
-        int bts = 1;
-        for (int f = a; f >= 1; f--) {
-            awal *= f;
-        }
-        int bwh = (a - b);
-        for (int h = bwh; h >= 1; h--) {
-            btsbwh *= h;
-        }
-        for (int j = b; j >= 1; j--) {
-            bts *= j;
-        }
-        int hasilkom = (awal / (btsbwh * bts));
-        return hasilkom;
+    public int kombinasi(int atas, int bawah) {
+        return faktorial(atas) / (faktorial((atas) - (bawah)) * faktorial(bawah));
+
+//        int awal = 1;
+//        int btsbwh = 1;
+//        int bts = 1;
+//        for (int f = a; f >= 1; f--) {
+//            awal *= f;
+//        }
+//        int bwh = (a - b);
+//        for (int h = bwh; h >= 1; h--) {
+//            btsbwh *= h;
+//        }
+//        for (int j = b; j >= 1; j--) {
+//            bts *= j;
+//        }
+//        int hasilkom = (awal / (btsbwh * bts));
+//        return hasilkom;
     }
 
     /**
@@ -49,101 +52,106 @@ public class Nomor1 {
     public double pangkat(double a, double b) {
         double hasilpem = 1;
         double z = 1;
-        if (b > 0) {
-            while (z <= b) {
-                hasilpem = hasilpem * a;
-                z++;
-            }
-        } else {
-            while (z <= Math.abs(b)) {
-                hasilpem = hasilpem / a;
-                z++;
-            }
+        while (z <= b) {
+//            if (b > 0) {
+//                hasilpem *= a;
+//            } else {
+//                hasilpem /= a;
+//            }
+            hasilpem=(b > 0) ? hasilpem * a : hasilpem / a;
+
+            z++;
         }
 
+//        String nama = (true)?"dave":"lain";
+        
+        
+//        if (b > 0) {
+//            while (z <= b) {
+//                hasilpem = hasilpem * a;
+//                z++;
+//            }
+//        } else {
+//            while (z <= Math.abs(b)) {
+//                hasilpem = hasilpem / a;
+//                z++;
+//            }
+//        }
         return hasilpem;
     }
+
     /**
-    *
-    * @author erik
-    */
-     /**
-     * digunakan untuk melakukan perkalian sesuai dengan batas faktorial (b)
-     * return digunakan untuk mengembalikan nilai hasil ke dalam perkalian,sampai nilainya benar benar 0
-     * @param fak1
-     * @return 
+     *
+     * @author erik
      */
-    public static int faktor(int fak1) {
-        int hasil =fak1;
-        for (int b=1;b<fak1;b++) {
-         hasil = (fak1-b) * hasil;
-        }    
+    /**
+     * digunakan untuk melakukan perkalian sesuai dengan batas faktorial (b)
+     * return digunakan untuk mengembalikan nilai hasil ke dalam
+     * perkalian,sampai nilainya benar benar 0
+     *
+     * @param base
+     * @return
+     */
+    public static int faktorial(int base) {
+        int hasil = 1;
+        for (int b = base; b >= 1; b--) {
+            hasil *= b;
+        }
         return hasil;
     }
-    
+
     /**
-    *
-    * @author erik
-    */
-    
-    /**
-     * 
-     * @param x digunakan untuk melakukan penandaan berapa kali looping dilakukan 
-     * @param a angka yang digunakan untuk melakuka penjumlahan dengan hasil sesuai dari looping x
-     * @return  digunakan untuk mengembalikan nilai hasil ke dalam looping sampai looping habis
+     *
+     * @author erik
      */
-    public static int kali(int x,int a) {
-   int hasil=0;
-    for (int c=1;c<=Math.abs(x);c++) {
-        if (x<0){
-            hasil=hasil-a;
+    /**
+     *
+     * @param x digunakan untuk melakukan penandaan berapa kali looping
+     * dilakukan
+     * @param a angka yang digunakan untuk melakuka penjumlahan dengan hasil
+     * sesuai dari looping x
+     * @return digunakan untuk mengembalikan nilai hasil ke dalam looping sampai
+     * looping habis
+     */
+    public static int kali(int x, int a) {
+        int hasil = 0;
+        for (int c = 1; c <= Math.abs(x); c++) {
+            if (x < 0) {
+                hasil -= a;
+            } else {
+                hasil += a;
+            }
         }
-        else {
-        hasil=hasil+a;
-    }     
+        return hasil;
     }
-    return hasil;
-}
-    
-     /**
-    *
-    * @author erik
-    */
-    
+
     /**
-     * 
-     * @param atas digunakan untuk menjadi angka yang dilakukan perkalian looping 
-     * @param bawah digunakan untuk mencari batas dari looping yang dilakukan pada angka atas dengan 
-     * @return 
+     *
+     * @author erik
      */
-    public static int permut(int atas, int bawah){
-        
-        int hasil=1;
-        for (int c=0;c<bawah;c++) {
-            hasil = (atas-c) *hasil;
-            /**
-             * int hasil digunakan untuk mendeklarasikan nilai hasil awal yaitu 1
-             * for digunakan untuk melakukan looping perkalian sesuai dengan banyaknya nilai faktorial
-             */
-        }
-        return hasil; 
-         /**
-         * digunakan untuk mengembalikan nilai dari hasil
-         */
+    /**
+     *
+     * @param atas digunakan untuk menjadi angka yang dilakukan perkalian
+     * looping
+     * @param bawah digunakan untuk mencari batas dari looping yang dilakukan
+     * pada angka atas dengan
+     * @return
+     */
+    public static int permutasi(int atas, int bawah) {
+        return faktorial(atas) / faktorial(atas - bawah);
     }
 
     public static void main(String[] args) {
         Nomor1 fungsi = new Nomor1();
         System.out.println("Hasil Pemangkatan : " + fungsi.pangkat(5, 4));
-        System.out.println("Hasil Pemangkatan : " + fungsi.pangkat(5, -4));
+        System.out.println("Hasil Pemangkatan : " + fungsi.pangkat(5, 0));
         System.out.println("Hasil Kombinasi : " + fungsi.kombinasi(5, 4));
-        System.out.println("Hasil Kombinasi : " + fungsi.kombinasi(5, 4));
-        System.out.println("Hasil Kombinasi : " + fungsi.faktor(8));
-        System.out.println("Hasil Kombinasi : " + fungsi.kali(5,4));
-        System.out.println("Hasil Kombinasi : " + fungsi.kali(-5,4));
-        System.out.println("Hasil Kombinasi : " + fungsi.kali(5,-4));
-        System.out.println("Hasil Kombinasi : " + fungsi.kali(-5,-4));
-        System.out.println("Hasil Kombinasi : " + fungsi.permut(5,4));
+        System.out.println("Hasil faktorial : " + fungsi.faktorial(0));
+        System.out.println("Hasil Perkalian : " + fungsi.kali(5, 4));
+        System.out.println("Hasil Perkalian : " + fungsi.kali(-5, 4));
+        System.out.println("Hasil Perkalian : " + fungsi.kali(5, -4));
+        System.out.println("Hasil Perkalian : " + fungsi.kali(-5, -4));
+        System.out.println("Hasil Permutasi : " + fungsi.permutasi(5, 2));
 
     }
 
