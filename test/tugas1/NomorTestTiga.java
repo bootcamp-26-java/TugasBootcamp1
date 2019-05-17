9f1532e8ce7554ebcecf7606a1086d8ce413d34f
savitrisekar <savitrisekar@gmail.com>
2019-05-17 08:23:35 +0700

    [sekar] update nomo tigaa

---
+++ test/tugas1/NomorTestTiga.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas1;
+
+import java.util.Arrays;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Sekar Ayu Safitri
+ */
+public class NomorTestTiga {
+    
+    public NomorTestTiga() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+     @Test
+    public void isPrima(){
+         assertEquals(Arrays.asList(2, 3, 5, 7), NomorTiga.prima(10));
+    }
+}
