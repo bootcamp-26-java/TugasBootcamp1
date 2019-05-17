7105a05bd3911fbdb9c10e1603846df6a8c2495d
PutraPradana <ppradana92@gmail.com>
2019-05-17 09:04:22 +0700

    Dan! Update Test Nomor 4

--- test/tugas1/Nomor4Test.java
+++ test/tugas1/Nomor4Test.java
@@ -1,23 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tugas1;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author WINDOWS 10
- */
+
 public class Nomor4Test {
     
     public Nomor4Test() {
     }
 
+    
     /**
+     * author Dan!
      * Test of Pecah method, of class Nomor4.
      */
     @Test
