362a841bc1dcef7cc21becf36b14c0e5fd53e8c4
ariffridasari <arif.fridasari@students.amikom.ac.id>
2019-05-17 07:44:20 +0700

    Arif Fridasari Menambah testing

--- test/TestNomor5.java
+++ test/TestNomor5.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 
+import java.util.Arrays;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import tugas1.Nomor5;
@@ -13,11 +14,11 @@ import tugas1.Nomor5;
  * @author Arif Fridasari
  */
 public class TestNomor5 {
-    Nomor5 nomor5;
     public TestNomor5() {
     }
      @Test
      public void test5() {
-         
+          Nomor5 obj = new Nomor5();
+          assertEquals(Arrays.asList("15","14","13","12","11","10","09","08","07","06","05","04","03"),obj.decrement(15,3));
      }
 }
